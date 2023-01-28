4d6d089fa5b159cdf9033f300ad2770f8325732b
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-01-28 09:57:26 +0700

    commit the frist 28/01/2023

---
+++ src/main/java/daothuan/travel/microservices/blogtour/BlogTourApplication.java
@@ -0,0 +1,13 @@
+package daothuan.travel.microservices.blogtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daothuan/travel/microservices/blogtour/BlogTourApplicationTests.java
@@ -0,0 +1,13 @@
+package daothuan.travel.microservices.blogtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
